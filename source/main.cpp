41b60562c13bbba12a0fa0d1c24a72b8c818b20c
rsc16 <robertchitty71@gmail.com>
2023-09-26 09:23:59 +1300

    LEDS uncomment please

--- source/main.cpp
+++ source/main.cpp
@@ -7,11 +7,19 @@ int main()
 {
     uBit.init();
 
+    //Motor Run is driving
+    //CW means forward
+    //0x80 means speed at 128 digital value
     motorRun(Motors::All, Dir::CW,0x80);
-
+    //Sleep for 2 seconds
     uBit.sleep(2000);
-
+    //0x0 means stop
     motorRun(Motors::All, Dir::CW,0x0);
-
+    //Make LEDS flash
+    /*writeLED(LED::LEDLeft, LEDswitch::turnOn);
+    writeLED(LED::LEDRight, LEDswitch:: turnOn);
+    uBit.sleep(2000);
+    writeLED(LED::LEDLeft, LEDswitch::turnOff);
+    writeLED(LED::LEDRight, LEDswitch:: turnOff);*/
 }
 
