5fa703060b61096d20ecf3ea8fd970e20520eb80
rsc16 <robertchitty71@gmail.com>
2023-10-02 09:32:13 +1300

    Turning - follow left side of the line, then the right side of the line

--- source/main.cpp
+++ source/main.cpp
@@ -7,34 +7,34 @@ MicroBit uBit;
 #define FOLLOW_FORWARD 0
 #define FOLLOW_RIGHT 1
 
-int follow_direction = FOLLOW_LEFT;
+int follow_direction = FOLLOW_FORWARD;
 
 void followLeft(int offLeft, int offRight){
     // white on left side, black on right side
     if(offLeft && !offRight){
         //right motor slightly faster than left motor
-        motorRun(Motors::Left, Dir::CW,0x30);
-        motorRun(Motors::Right, Dir::CW,0x80);
+        motorRun(Motors::Left, Dir::CW,0xC);
+        motorRun(Motors::Right, Dir::CW,0x20);
     }
     // black on left side, white on right side
     else if(!offLeft && offRight){
         // We have hit an intersection, turn left
         motorRun(Motors::Left, Dir::CW,0x0);
-        motorRun(Motors::Right, Dir::CW,0x80);
+        motorRun(Motors::Right, Dir::CW,0x20);
         uBit.sleep(100);
         //follow_direction = FOLLOW_RIGHT;
     }
     // both black
     else if(!offLeft && !offRight){
         // turn slightly left??
-        motorRun(Motors::Right, Dir::CW,0x80);
-        motorRun(Motors::Left, Dir::CW,0x60);
+        motorRun(Motors::Right, Dir::CW,0x20);
+        motorRun(Motors::Left, Dir::CW,0x18);
     }
     // both white
     else{
         // turn right
-        motorRun(Motors::Right, Dir::CW,0x10);
-        motorRun(Motors::Left, Dir::CW,0x80);
+        motorRun(Motors::Right, Dir::CW,0x4);
+        motorRun(Motors::Left, Dir::CW,0x20);
     }
 }
 void followRight(int offLeft, int offRight){
@@ -42,7 +42,7 @@ void followRight(int offLeft, int offRight){
     if(offLeft && !offRight){
         // We have hit an intersection, turn right
         motorRun(Motors::Right, Dir::CW,0x0);
-        motorRun(Motors::Left, Dir::CW,0x80);
+        motorRun(Motors::Left, Dir::CW,0x20);
         uBit.sleep(100);
         follow_direction = FOLLOW_LEFT;
         
@@ -50,25 +50,68 @@ void followRight(int offLeft, int offRight){
     // black on left side, white on right side
     else if(!offLeft && offRight){
         //left motor slightly faster than right motor
-        motorRun(Motors::Right, Dir::CW,0x30);
-        motorRun(Motors::Left, Dir::CW,0x80);
+        motorRun(Motors::Right, Dir::CW,0xC);
+        motorRun(Motors::Left, Dir::CW,0x20);
         
     }
     // both black
     else if(!offLeft && !offRight){
         // turn slightly right
-        motorRun(Motors::Left, Dir::CW,0x80);
-        motorRun(Motors::Right, Dir::CW,0x60);
+        motorRun(Motors::Left, Dir::CW,0x20);
+        motorRun(Motors::Right, Dir::CW,0x18);
     }
     // both white
     else{
         // turn left
-        motorRun(Motors::Left, Dir::CW,0x10);
-        motorRun(Motors::Right, Dir::CW,0x80);
+        motorRun(Motors::Left, Dir::CW,0x4);
+        motorRun(Motors::Right, Dir::CW,0x20);
     }
 }
 void followForward(int offLeft, int offRight){
+    if(offLeft && !offRight)
+    {
+        motorRun(Motors::Left, Dir::CW,0x20);
+        motorRun(Motors::Right, Dir::CW,0x0);
+    }
+    else if(!offLeft && offRight)
+    {
+        motorRun(Motors::Left, Dir::CW,0x0);
+        motorRun(Motors::Right, Dir::CW,0x20);
+    }
+    else if(!offLeft && !offRight)
+    {
+        motorRun(Motors::All, Dir::CW,0x20);
+    }
+    else
+    {
+        motorRun(Motors::All, Dir::CCW,0x10);
+    }
+    /*if(readPatrol(Patrol::PatrolLeft))
+        {
+            writeLED(LED::LEDLeft, LEDswitch::turnOn);
+            motorRun(Motors::Left, Dir::CW,0x80);
+        }
+        else
+        {
+            writeLED(LED::LEDLeft, LEDswitch::turnOff);
+            motorRun(Motors::Left, Dir::CW,0x0);
+        }
+        if(readPatrol(Patrol::PatrolRight))
+        {
+            writeLED(LED::LEDRight, LEDswitch::turnOn);
+            motorRun(Motors::Right, Dir::CW,0x80);
+        }
+        else
+        {
+            writeLED(LED::LEDRight, LEDswitch::turnOff);
+            motorRun(Motors::Right, Dir::CW,0x0);
+        }
 
+        if(!readPatrol(Patrol::PatrolLeft) && !readPatrol(Patrol::PatrolRight))
+        {
+            motorRun(Motors::All, Dir::CW,0x80);
+        }*/
+        uBit.sleep(20);
 }
 
 
@@ -104,13 +147,13 @@ int main()
         
 
         uBit.sleep(20);
-        int distance = readUlt();
+        /*int distance = readUlt();
         while(distance < 5 && distance >= 0) //3 centimetres away
         {
             motorRun(Motors::All, Dir::CW, 0x0);
             uBit.sleep(1000);
             distance = readUlt();
-        }
+        }*/
     }
     //Make LEDS flash
 
